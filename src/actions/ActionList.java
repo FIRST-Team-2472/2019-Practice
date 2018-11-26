7cad14dd08eaf9efa713ecd120f1e9e583d3f3a6
2472Centurions <2472centurions@gmail.com>
2018-11-26 16:58:05 -0600

    Half implmented ActionList

--- src/actions/ActionList.java
+++ src/actions/ActionList.java
@@ -1,5 +1,38 @@
 package actions;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+
 public class ActionList {
+	
+	private final Queue<Actionable> stepPrimary;
+	private final Queue<Actionable> stepSecondary;
+	
+	public ActionList() {
+		stepPrimary = new ArrayDeque<Actionable>();
+		stepSecondary = new ArrayDeque<Actionable>();
+	}
+	
+	public void addPrimaryAction(Actionable action) {
+		stepPrimary.add(action);
+	}
+	
+	public void addSecondaryAction(Actionable action) {
+		stepSecondary.add(action);
+	}
+	
+	public void step() {
+		try {
+			Actionable primary = stepPrimary.element();
+			Actionable secondary = stepSecondary.element();
+			
+		} catch (NoSuchElementException e) {
+			System.out.println("Nothing in queue I am done");
+		}
+	}
+	
 
 }
